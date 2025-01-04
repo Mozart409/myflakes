aa44774f2bc130f2b1c10629a36754c36008b419
Amadeus Mader <amadeus@mozart409.com>
2025-01-04 23:59:39 +0100

    feat: :sparkles: added nettools eza and openrgb

--- home.nix
+++ home.nix
@@ -30,8 +30,11 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     hello
-    git-credential-oauth
     neovim
+    git-credential-oauth
+    eza
+    nettools
+    openrgb-with-all-plugins
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -80,7 +83,6 @@
   home.sessionVariables = {
     EDITOR = "neovim";
   };
-
   programs.zsh = {
     enable = true;
     autocd = true;
