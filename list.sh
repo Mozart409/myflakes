e12f5dd9a5a656061d60e214145ec481bddfbc58
Amadeus Mader <amadeus@mozart409.com>
2025-01-10 17:56:26 +0100

    fix: :wrench: fixed list

--- list.sh
+++ list.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Listing generations"
+echo "Deleting generations..."
 
-nix-env --list-generations
+# nix-env --list-generations
 
-sudo nix-env -p /nix/var/nix/profiles/system --delete-generations -5
-
+sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +5
 exit 0
