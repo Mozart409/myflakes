c301eb261709bf7318f03325d7711139b7a20355
Amadeus Mader <amadeus@mozart409.com>
2025-02-02 17:49:26 +0100

    fix: :fire: just show generations

--- list.sh
+++ list.sh
@@ -2,7 +2,7 @@
 
 echo "Deleting generations..."
 
-# nix-env --list-generations
-
-sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +5
+nix-env --list-generations
+# sudo nix-collect-garbage -d
+# sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +5
 exit 0
