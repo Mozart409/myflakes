5cb5e3e1e3ffd39fba66b0a8ac9f122831ee50e1
Amadeus Mader <amadeus@mozart409.com>
2025-02-04 22:40:18 +0100

    fix: :sparkles: added trusted-users

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,10 @@
 
   nix.gc.automatic = true;
 
+  nix.extraOptions = ''
+    trusted-users = root amadeus
+  '';
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
