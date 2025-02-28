1f56d1a1350e2edb962bb86079ff1103a2be9744
Amadeus Mader <amadeus@mozart409.com>
2025-02-28 20:07:58 +0100

    feat: :wrench: enabled ipv6

--- configuration.nix
+++ configuration.nix
@@ -194,7 +194,8 @@
         userland-proxy = false;
         experimental = true;
         metrics-addr = "0.0.0.0:9323";
-        ipv6 = false;
+        ipv6 = true;
+        fixed-cidr-v6 = "fd00::/80";
       };
     };
   };
